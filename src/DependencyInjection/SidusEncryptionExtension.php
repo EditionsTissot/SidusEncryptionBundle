c30c6d2674a1ed2f2e710d40104253791f594e3e
johnkrovitch <arnaudfrezet@gmail.com>
2020-11-27 17:00:39 +0100

    Add the encrypt Doctrine types automatically

--- src/DependencyInjection/SidusEncryptionExtension.php
+++ src/DependencyInjection/SidusEncryptionExtension.php
@@ -10,9 +10,12 @@
 
 namespace Sidus\EncryptionBundle\DependencyInjection;
 
+use Sidus\EncryptionBundle\Doctrine\Type\EncryptStringType;
+use Sidus\EncryptionBundle\Doctrine\Type\EncryptTextType;
 use Sidus\EncryptionBundle\Registry\EncryptionManagerRegistry;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -23,12 +26,13 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @author Vincent Chalnot <vincent@sidus.fr>
  */
-class SidusEncryptionExtension extends Extension
+class SidusEncryptionExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/services'));
         $loader->load('encryption.yml');
+        $loader->load('doctrine.yml');
         $loader->load('registry.yml');
         $loader->load('security.yml');
         $loader->load('session.yml');
@@ -39,5 +43,23 @@ class SidusEncryptionExtension extends Extension
         $registry = $container->getDefinition(EncryptionManagerRegistry::class);
         $registry->replaceArgument('$defaultCode', $config['preferred_adapter']);
         $container->setParameter('sidus.encryption.throw_exceptions', $config['throw_exception']);
+    }
+    
+    public function prepend(ContainerBuilder $container)
+    {
+        $doctrineConfiguration = $container->getExtensionConfig('doctrine');
+    
+        if (empty($doctrineConfiguration['dbal'])) {
+            $doctrineConfiguration['dbal'] = [];
+        }
+    
+        if (empty($doctrineConfiguration['dbal']['types'])) {
+            $doctrineConfiguration['dbal']['types'] = [];
+        }
+        $doctrineConfiguration['dbal']['types'] += [
+            'encrypt_string' => EncryptStringType::class,
+            'encrypt_text' => EncryptTextType::class
+        ];
+        $container->prependExtensionConfig('doctrine', $doctrineConfiguration);
     }
 }
